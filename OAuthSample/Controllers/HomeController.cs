885d0c334e86a0fb236c5e21bc722060928e8409
Slobodan Pavkov <Slobodan Pavkov>
2016-07-24 00:45:14 +0200

    Initial commit

---
+++ OAuthSample/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OAuthSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.AppId = System.Configuration.ConfigurationManager.AppSettings["AppId"];
+            ViewBag.CallbackUrl = System.Configuration.ConfigurationManager.AppSettings["CallbackUrl"];
+            ViewBag.Scope = System.Configuration.ConfigurationManager.AppSettings["Scope"];
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+          
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ OAuthSample/Controllers/OAuthController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using OAuthSample.Models;
+
+namespace OAuthSample.Controllers
+{
+    public class OAuthController : Controller
+    {
+        //
+        // GET: /OAuth/
+        public ActionResult Index()
+        {
+
+            return View();
+
+        }
+
+        public ActionResult RequestToken(string code, string status)
+        {
+            return new RedirectResult(GenerateAuthorizeUrl());
+        }
+
+        public ActionResult RefreshToken(string refreshToken)
+        {
+            TokenModel token = new TokenModel();
+            String error = null;
+
+            if (!String.IsNullOrEmpty(refreshToken))
+            {
+                error = PerformTokenRequest(GenerateRefreshPostData(refreshToken), out token);
+                if (String.IsNullOrEmpty(error))
+                {
+                    ViewBag.Token = token;
+                }
+            }
+
+            ViewBag.Error = error;
+
+            return View("TokenView");
+        }
+
+        public ActionResult Callback(string code, string state)
+        {
+            TokenModel token = new TokenModel();
+            String error = null;
+
+            if (!String.IsNullOrEmpty(code))
+            {
+                error = PerformTokenRequest(GenerateRequestPostData(code), out token);
+                if (String.IsNullOrEmpty(error))
+                {
+                    ViewBag.Token = token;
+                }
+            }
+
+            ViewBag.Error = error;
+
+            return View("TokenView");
+        }
+
+        private String PerformTokenRequest(String postData, out TokenModel token)
+        {
+            var error = String.Empty;
+            var strResponseData = String.Empty;
+
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(
+                ConfigurationManager.AppSettings["TokenUrl"]
+                );
+
+            webRequest.Method = "POST";
+            webRequest.ContentLength = postData.Length;
+            webRequest.ContentType = "application/x-www-form-urlencoded";
+
+            using (StreamWriter swRequestWriter = new StreamWriter(webRequest.GetRequestStream()))
+            {
+                swRequestWriter.Write(postData);
+            }
+
+            try
+            {
+                HttpWebResponse hwrWebResponse = (HttpWebResponse)webRequest.GetResponse();
+
+                if (hwrWebResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    using (StreamReader srResponseReader = new StreamReader(hwrWebResponse.GetResponseStream()))
+                    {
+                        strResponseData = srResponseReader.ReadToEnd();
+                    }
+
+                    token = JsonConvert.DeserializeObject<TokenModel>(strResponseData);
+                    return null;
+                }
+            }
+            catch (WebException wex)
+            {
+                error = "Request Issue: " + wex.Message;
+            }
+            catch (Exception ex)
+            {
+                error = "Issue: " + ex.Message;
+            }
+
+            token = new TokenModel();
+            return error;
+        }
+
+        public String GenerateAuthorizeUrl()
+        {
+            UriBuilder uriBuilder = new UriBuilder(ConfigurationManager.AppSettings["AuthUrl"]);
+            var queryParams = HttpUtility.ParseQueryString(uriBuilder.Query ?? String.Empty);
+
+            queryParams["client_id"] = ConfigurationManager.AppSettings["AppId"];
+            queryParams["response_type"] = "Assertion";
+            queryParams["state"] = "state";
+            queryParams["scope"] = ConfigurationManager.AppSettings["Scope"];
+            queryParams["redirect_uri"] = ConfigurationManager.AppSettings["CallbackUrl"];
+
+            uriBuilder.Query = queryParams.ToString();
+
+            return uriBuilder.ToString();
+        }
+
+        public string GenerateRequestPostData(string code)
+        {
+            return string.Format("client_assertion_type=urn:ietf:params:oauth:client-assertion-type:jwt-bearer&client_assertion={0}&grant_type=urn:ietf:params:oauth:grant-type:jwt-bearer&assertion={1}&redirect_uri={2}",
+                HttpUtility.UrlEncode(ConfigurationManager.AppSettings["AppSecret"]),
+                HttpUtility.UrlEncode(code),
+                ConfigurationManager.AppSettings["CallbackUrl"]
+                );
+        }
+
+        public string GenerateRefreshPostData(string refreshToken)
+        {
+            return string.Format("client_assertion_type=urn:ietf:params:oauth:client-assertion-type:jwt-bearer&client_assertion={0}&grant_type=refresh_token&assertion={1}&redirect_uri={2}",
+                HttpUtility.UrlEncode(ConfigurationManager.AppSettings["AppSecret"]),
+                HttpUtility.UrlEncode(refreshToken),
+                ConfigurationManager.AppSettings["CallbackUrl"]
+                );
+
+        }
+    }
+}
